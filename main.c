80a58eaf4d3105c2c6c39604b065c17b2b9b9a08
kansanxiao1102 <nysm0715@gmail.com>
2018-05-18 10:03:28 +0800

    test1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int sum (int init,int endpointt);
+
+int main(int argc, char *argv[]) {
+	// »¼¥[½m²ß 
+
+	printf("summary=%d\n",sum(5,10));
+	return 0;
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+int sum (int init,int endpoint)
+{
+	int summary;
+	for(init=0,summary=0;init<endpoint;init++)
+	summary=summary+init;
+	return summary;
+	}	
+	
+	
+	
